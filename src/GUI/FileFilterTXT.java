b6424611756c949e145c9b6c8dc1798801d99db5
Eymar Lima <eymarlima@gmail.com>
2017-09-19 11:51:40 -0300

    GUI Inicio

---
+++ src/GUI/FileFilterTXT.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import java.io.File;
+import javax.swing.filechooser.FileFilter;
+
+/**
+ *
+ * @author YiazmaT
+ */
+public class FileFilterTXT extends FileFilter{
+    @Override
+    public boolean accept(File f) {
+        return f.isDirectory() || f.getName().endsWith(".txt");
+    }
+
+    @Override
+    public String getDescription() {
+        return ".txt";
+    }
+}
